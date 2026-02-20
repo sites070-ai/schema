c69d91df006bed9f3a4ce67ea8d8cb17a1037d06
sites070-ai <sites070@gmail.com>
2026-02-20 13:06:08 +0100

    Create piatti-menu-pranzo.js

---
+++ piatti-menu-pranzo.js
@@ -0,0 +1,14 @@
+let TOP_PIATTI = {
+  "orecchiette al pesto":{categoria:"primo",regione:"Puglia",iconico:false,top:true,storico:false,punteggio:4,sinonimi:["orecchiette al pesto di basilico"],allergeni:["glutine","latte"],dieta:"vegetariano"},
+  "tortelli di carne panna e prosciutto":{categoria:"primo",regione:"Emilia-Romagna",iconico:false,top:true,storico:false,punteggio:4,sinonimi:["tortelli panna e prosciutto"],allergeni:["glutine","latte","uova"],dieta:"carne"},
+  "spaghetti al pomodoro e basilico":{categoria:"primo",regione:"Italia",iconico:true,top:true,storico:true,punteggio:5,sinonimi:["spaghetti al pomodoro"],allergeni:["glutine"],dieta:"vegetariano"},
+
+  "arrosto di coppa e lonza con patate al forno":{categoria:"secondo",regione:"Emilia-Romagna",iconico:false,top:true,storico:false,punteggio:4,sinonimi:["arrosto misto di maiale"],allergeni:[],dieta:"carne"},
+  "bistecche alla pizzaiola":{categoria:"secondo",regione:"Campania",iconico:false,top:true,storico:true,punteggio:4,sinonimi:["fettine alla pizzaiola"],allergeni:[],dieta:"carne"},
+  "coppa alla griglia":{categoria:"secondo",regione:"Emilia-Romagna",iconico:false,top:true,storico:false,punteggio:4,sinonimi:["coppa di maiale alla griglia"],allergeni:[],dieta:"carne"},
+
+  "zucchine trifolate":{categoria:"contorno",regione:"Italia",iconico:false,top:true,storico:true,punteggio:4,sinonimi:["zucchine in padella"],allergeni:[],dieta:"vegetariano"},
+  "insalata mista":{categoria:"contorno",regione:"Italia",iconico:false,top:false,storico:true,punteggio:3,sinonimi:["insalata verde"],allergeni:[],dieta:"vegetariano"},
+  "mix di verdure lessate":{categoria:"contorno",regione:"Italia",iconico:false,top:false,storico:true,punteggio:3,sinonimi:["verdure bollite"],allergeni:[],dieta:"vegetariano"}
+};
+
