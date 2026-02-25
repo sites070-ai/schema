a6a72a9d365a30130ab9a61b49f290a2b2ac9edb
sites070-ai <sites070@gmail.com>
2026-02-25 11:14:37 +0100

    Update piatti-menu-pranzo.js

--- piatti-menu-pranzo.js
+++ piatti-menu-pranzo.js
@@ -12,18 +12,26 @@ let TOP_PIATTI = {
   "bistecca alla griglia": {categoria:"secondo", regione:"Italia", iconico:true, top:true, selezione:false, punteggio:4, sinonimi:["bistecca ai ferri"], allergeni:[], dieta:"carne"},
   "bistecche alla pizzaiola": {categoria:"secondo", regione:"Campania", iconico:false, top:true, selezione:true, punteggio:4, sinonimi:["fettine alla pizzaiola"], allergeni:[], dieta:"carne"},
   "coppa alla griglia": {categoria:"secondo", regione:"Emilia-Romagna", iconico:false, top:true, selezione:false, punteggio:4, sinonimi:["coppa di maiale alla griglia"], allergeni:[], dieta:"carne"},
+  "grigliata mista salsiccia-coppa-wurstel": {categoria:"secondo", regione:"Italia", iconico:false, top:true, selezione:false, punteggio:4, sinonimi:["grigliata mista","grigliata di carne"], allergeni:[], dieta:"carne"},
   "guanciale in umido con polenta e purè": {categoria:"secondo", regione:"Lombardia", iconico:false, top:true, selezione:false, punteggio:4, sinonimi:["guanciale in umido","guanciale con polenta"], allergeni:["latte"], dieta:"carne"},
-  "insalata mista": {categoria:"contorno", regione:"Italia", iconico:false, top:false, selezione:true, punteggio:3, sinonimi:["insalata verde"], allergeni:[], dieta:"vegetariano"},
+  "insalata mista": {categoria:"contorno", regione:"Italia", iconico:false, top:false, selezione:true, punteggio:3, sinonimi:["insalata verde mista"], allergeni:[], dieta:"vegetariano"},
+  "insalata verde": {categoria:"contorno", regione:"Italia", iconico:false, top:false, selezione:true, punteggio:3, sinonimi:["insalata semplice"], allergeni:[], dieta:"vegetariano"},
   "mix di verdure lessate (fagiolini-broccoli-carote-patate)": {categoria:"contorno", regione:"Italia", iconico:false, top:false, selezione:true, punteggio:3, sinonimi:["mix di verdure lessate","verdure bollite","verdure lessate"], allergeni:[], dieta:"vegetariano"},
   "orecchiette al pesto": {categoria:"primo", regione:"Puglia", iconico:false, top:true, selezione:false, punteggio:4, sinonimi:["orecchiette al pesto di basilico"], allergeni:["glutine","latte"], dieta:"vegetariano"},
+  "pisarei e fasoi": {categoria:"primo", regione:"Emilia-Romagna", iconico:true, top:true, selezione:false, punteggio:5, sinonimi:["pisarei e faso"], allergeni:["glutine"], dieta:"vegetariano"},
+  "pollo arrosto con patate": {categoria:"secondo", regione:"Italia", iconico:true, top:true, selezione:false, punteggio:4, sinonimi:["pollo al forno con patate"], allergeni:[], dieta:"carne"},
   "pomodori": {categoria:"contorno", regione:"Italia", iconico:false, top:false, selezione:true, punteggio:3, sinonimi:["pomodori freschi","pomodori al piatto"], allergeni:[], dieta:"vegetariano"},
   "spaghetti alla norma (pomodoro-melanzane-ricotta)": {categoria:"primo", regione:"Sicilia", iconico:true, top:true, selezione:true, punteggio:5, sinonimi:["spaghetti alla norma","pasta alla norma"], allergeni:["glutine","latte"], dieta:"vegetariano"},
   "spaghetti al pomodoro e basilico": {categoria:"primo", regione:"Italia", iconico:true, top:true, selezione:true, punteggio:5, sinonimi:["spaghetti al pomodoro"], allergeni:["glutine"], dieta:"vegetariano"},
+  "spaghetti pomodoro e basilico": {categoria:"primo", regione:"Italia", iconico:true, top:true, selezione:true, punteggio:5, sinonimi:["spaghetti al pomodoro e basilico","spaghetti al pomodoro"], allergeni:["glutine"], dieta:"vegetariano"},
   "spiedino misto (pollo-salsiccia-lonza)": {categoria:"secondo", regione:"Italia", iconico:false, top:true, selezione:false, punteggio:4, sinonimi:["spiedino misto","spiedino di carne"], allergeni:[], dieta:"carne"},
+  "tacchino tonnato": {categoria:"secondo", regione:"Piemonte", iconico:true, top:true, selezione:true, punteggio:5, sinonimi:["tacchino con salsa tonnata"], allergeni:["uova","pesce"], dieta:"carne"},
   "tortelli di carne panna e prosciutto": {categoria:"primo", regione:"Emilia-Romagna", iconico:false, top:true, selezione:false, punteggio:4, sinonimi:["tortelli panna e prosciutto"], allergeni:["glutine","latte","uova"], dieta:"carne"},
   "tortiglioni al ragù": {categoria:"primo", regione:"Emilia-Romagna", iconico:true, top:true, selezione:false, punteggio:4, sinonimi:["pasta al ragù","tortiglioni al ragù di carne"], allergeni:["glutine"], dieta:"carne"},
+  "tortiglioni con crema di broccoli e speck": {categoria:"primo", regione:"Italia", iconico:false, top:true, selezione:false, punteggio:4, sinonimi:["pasta broccoli e speck","tortiglioni broccoli e speck"], allergeni:["glutine","latte"], dieta:"carne"},
   "zucchine trifolate": {categoria:"contorno", regione:"Italia", iconico:false, top:true, selezione:true, punteggio:4, sinonimi:["zucchine in padella"], allergeni:[], dieta:"vegetariano"}
 };
+
 
 
 
